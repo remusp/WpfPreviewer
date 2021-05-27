6f0990bebd059cf721080e0bb1992cf2a445aaf2
remus <remus@BOREALIS>
2021-05-27 22:24:07 +0300

    Added delete button

--- XamlResourcePreviewer/XamlResourcePreviewer/MainWindowViewModel.cs
+++ XamlResourcePreviewer/XamlResourcePreviewer/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Input;
 using XamlResourcePreviewer.Logic;
 
 namespace XamlResourcePreviewer
@@ -14,7 +15,10 @@ namespace XamlResourcePreviewer
         {
             Files = new ObservableCollection<ResourceDictionaryItem>();
             _xamlService = xamlService;
+            RemoveFileCommand = new DelegateCommand<ResourceDictionaryItem>(file => RemoveFileCommandExecute(file));
         }
+
+        public ICommand RemoveFileCommand { get; }
 
         public ObservableCollection<ResourceDictionaryItem> Files
         {
@@ -43,5 +47,11 @@ namespace XamlResourcePreviewer
                 RenderedItem = r.Value
             }).ToList();
         }
+
+        private void RemoveFileCommandExecute(ResourceDictionaryItem file)
+        {
+            Files.Remove(file);
+        }
+
     }
 }
